2fe94f4aa1c085268fee0986e57797d93dcaf49e
Terence Chiu <tchiu2@gmail.com>
2019-09-04 12:32:11 -0400

    add Docker config

---
+++ server/src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const clientBuildPath = path.join(__dirname, '../client/build');
+
+app.use(express.static(clientBuildPath));
+
+app.get('*', (req, res) => res.sendFile(path.join(clientBuildPath, 'index.html')));
+
+app.listen(port, () => console.log(`Running on port ${port}`));
