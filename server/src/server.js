6ae34a90f1c13920677752884d068c74bcaf6e95
Terence Chiu <tchiu2@gmail.com>
2019-09-04 14:12:45 -0400

    update server

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-const clientBuildPath = path.join(__dirname, "../client/build");
+const clientBuildPath = path.join(__dirname, '../client/build');
 
 app.use(express.static(clientBuildPath));
 
-app.get("*", (req, res) =>
-  res.sendFile(path.join(clientBuildPath, "index.html"))
+app.get('*', (req, res) =>
+  res.sendFile(path.join(clientBuildPath, 'index.html'))
 );
 
 app.listen(port, () => console.log(`Running on port ${port}`));
