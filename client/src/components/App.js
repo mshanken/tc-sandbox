4d374ea46b70ccef36724caa1ab8de857fb9f77f
Terence Chiu <tchiu2@gmail.com>
2019-09-06 18:01:42 -0400

    add form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import '../stylesheets/App.css';
+import { Layout } from 'antd';
+import Form from './Form';
+
+const { Content, Header } = Layout;
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <Layout>
+      <Header />
+      <Content
+        style={{
+          padding: '0 5em',
+          marginTop: '1em',
+          width: '80%'
+        }}
+      >
+        <Form />
+      </Content>
+    </Layout>
+  );
 }
 
 export default App;
